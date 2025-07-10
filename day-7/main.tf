d37c248c442e0c1a91b0915693ebcc4b423b2f9c
Seema101095 <seemakumari10oct@gmail.com>
2025-07-10 17:39:16 +0530

    dynamodb

---
+++ day-7/main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "myvpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      name= "myvpc"
+    }
+}
+
+resource "aws_subnet" "subnet1" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.1.0/24"
+    tags ={
+        name="subnet1"
+    }
+    depends_on = [ aws_vpc.myvpc ]
+
+  
+}
+
+resource "aws_subnet" "subnet2" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = "10.0.2.0/24"
+    tags = {
+      name="subnet2"
+    }
+    depends_on = [ aws_vpc.myvpc ]
+}
